12b2dc5ceeaafe1770ffc626c7c2697ecbfbb66a
Yemel Jardi <angel.jardi@gmail.com>
2015-04-07 14:32:10 -0300

    Add setup and licence

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'coinage',
+  packages = ['coinage'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python bitcoin library',
+  author = 'Yemel Jardi',
+  author_email = 'angel.jardi@gmail.com',
+  url = 'https://github.com/yemel/coinage', # use the URL to the github repo
+  download_url = 'https://github.com/yemel/coinage/tarball/0.1', # I'll explain this in a second
+  keywords = ['bitcoin', 'altcoin'], # arbitrary keywords
+  classifiers = [],
+)
