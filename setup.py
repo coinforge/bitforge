84481aa7569ab21b9b2d37fbad0b8bff42d01dd8
Santiago Lezica <slezica89@gmail.com>
2015-04-14 21:58:18 -0300

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'bitforge',
-  packages = ['bitforge'], # this must be the same as the name above
+  packages = ['bitforge', 'bitforge.utils'], # this must be the same as the name above
   version = '0.3',
   description = 'A python bitcoin library',
   author = 'Yemel Jardi',
@@ -10,4 +10,5 @@ setup(
   download_url = 'https://github.com/muun/bitforge/tarball/0.3', # I'll explain this in a second
   keywords = ['bitcoin', 'altcoin'], # arbitrary keywords
   classifiers = [],
+  install_requires = ['enum34==1.0.4', 'ecdsa==0.13', 'pytest==2.7.0', 'nose==1.3.6']
 )
