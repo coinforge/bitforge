e971d961b76762e907cdd1652ea33a3e3926d542
Yemel Jardi <angel.jardi@gmail.com>
2015-04-14 21:01:39 -0300

    Transfer repo to muun organization

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'bitforge',
   packages = ['bitforge'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.3',
   description = 'A python bitcoin library',
   author = 'Yemel Jardi',
   author_email = 'angel.jardi@gmail.com',
-  url = 'https://github.com/yemel/bitforge', # use the URL to the github repo
-  download_url = 'https://github.com/yemel/bitforge/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/muun/bitforge', # use the URL to the github repo
+  download_url = 'https://github.com/muun/bitforge/tarball/0.3', # I'll explain this in a second
   keywords = ['bitcoin', 'altcoin'], # arbitrary keywords
   classifiers = [],
 )
