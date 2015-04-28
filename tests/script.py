c8f7ff42cacbb4426873c74ec6f18751a77be791
Santiago Lezica <slezica89@gmail.com>
2015-04-27 21:19:58 -0300

    Added script test

--- tests/script.py
+++ tests/script.py
@@ -1,8 +1,16 @@
-import pytest
+import pytest, inspect
+from pytest import raises, fixture, fail
 
-from bitforge.script import Script
-from bitforge.opcode import Opcode
+from bitforge import opcode
+from bitforge.script import Script, Instruction
+from bitforge.opcode import *
+from bitforge.encoding import *
+from bitforge.tools import Buffer
 
+
+class FakeString(str):
+    def __len__(self):
+        return super(FakeString, self).__len__() * 10
 
 
 class TestScript:
@@ -10,19 +18,62 @@ class TestScript:
         s = Script()
         assert s.instructions == []
 
-    def test_from_string(self):
-        s = 'OP_0 OP_PUSHDATA4 3 0x010203 OP_0'
-        assert Script.from_string(s).to_string() == s
+    def test_binary_single(self):
+        s = Script.from_bytes('\0')
+        assert s.instructions == [Instruction(OP_0)]
 
-        s = 'OP_0 OP_PUSHDATA2 3 0x010203 OP_0'
-        assert Script.from_string(s).to_string() == s
+    def test_binary_const_pushes(self):
+        for length in xrange(1, 76):
+            opcode = Opcode(length)
+            string = 'a' * length
 
-        s = 'OP_0 OP_PUSHDATA1 3 0x010203 OP_0'
-        assert Script.from_string(s).to_string() == s
+            s = Script.from_bytes(chr(length) + string)
 
-        s = 'OP_0 3 0x010203 OP_0'
-        assert Script.from_string(s).to_string() == s
+            assert s.instructions == [Instruction(opcode, string)]
 
-    def test_to_string_empty(self):
-        s = Script()
-        assert s.to_string() == ''
+    def test_binary_var_pushes(self):
+        s1 = Script.from_bytes(chr(OP_PUSHDATA1.number) + '\3' + 'abc')
+        assert s1.instructions == [Instruction(OP_PUSHDATA1, 'abc')]
+
+        s2 = Script.from_bytes(chr(OP_PUSHDATA2.number) + '\3\0' + 'abc')
+        assert s2.instructions == [Instruction(OP_PUSHDATA2, 'abc')]
+
+        s2 = Script.from_bytes(chr(OP_PUSHDATA4.number) + '\3\0\0\0' + 'abc')
+        assert s2.instructions == [Instruction(OP_PUSHDATA4, 'abc')]
+
+        with raises(Buffer.InsufficientData):
+            Script.from_bytes(chr(OP_PUSHDATA1.number) + '\3' + 'a')
+
+        with raises(Buffer.InsufficientData):
+            Script.from_bytes(chr(OP_PUSHDATA2.number) + '\3\0' + 'a')
+
+        with raises(Buffer.InsufficientData):
+            Script.from_bytes(chr(OP_PUSHDATA4.number) + '\3\0\0\0' + 'a')
+
+    def test_binary_all_nonpush_opcodes(self):
+        opcodes = []
+        for name, value in inspect.getmembers(opcode):
+            if isinstance(value, Opcode) and not value.is_push():
+                opcodes.append(value)
+
+        bytes = ''.join(chr(opcode.number) for opcode in opcodes)
+
+        s = Script.from_bytes(bytes)
+        assert s.instructions == map(Instruction, opcodes)
+
+    # def test_from_string(self):
+    #     s = 'OP_0 OP_PUSHDATA4 3 0x010203 OP_0'
+    #     assert Script.from_string(s).to_string() == s
+    #
+    #     s = 'OP_0 OP_PUSHDATA2 3 0x010203 OP_0'
+    #     assert Script.from_string(s).to_string() == s
+    #
+    #     s = 'OP_0 OP_PUSHDATA1 3 0x010203 OP_0'
+    #     assert Script.from_string(s).to_string() == s
+    #
+    #     s = 'OP_0 3 0x010203 OP_0'
+    #     assert Script.from_string(s).to_string() == s
+    #
+    # def test_to_string_empty(self):
+    #     s = Script()
+    #     assert s.to_string() == ''
