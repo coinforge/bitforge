78e5b0ff9da92aaa7e1ef8be58dafc36e03ecfaa
Federico Bond <federicobond@gmail.com>
2016-03-14 11:45:41 -0300

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -18,5 +18,19 @@ setuptools.setup(
   tests_require=['pytest'],
   install_requires = ['ecdsa==0.13'] + (['enum34==1.0.4'] if sys.version_info < (3, 4) else []),
 
-  classifiers = [],
+  classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Topic :: Internet',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+  ],
 )
