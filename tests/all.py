7f63640753ede18a558c7e4bf1c276693455acfb
Federico Bond <federicobond@gmail.com>
2016-03-14 12:07:35 -0300

    Use standard py.test conventions

--- tests/all.py
+++
@@ -1,7 +0,0 @@
-from .address import *
-from .privkey import *
-from .pubkey import *
-from .script import *
-from .transaction import *
-from .unit import *
-from .uri import *
--- tests/script/__init__.py
+++
@@ -1,2 +0,0 @@
-from .interpreter import *
-from .script import *
